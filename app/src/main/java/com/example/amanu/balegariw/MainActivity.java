343cceb194cd529e52a6f3df1e5ca90b942d8a85
Amanuel Tesfaye <amanueltesfaye44@gmail.com>
2019-07-03 07:06:17 -0400

    Navigation drawer is added.

--- app/src/main/java/com/example/amanu/balegariw/MainActivity.java
+++ app/src/main/java/com/example/amanu/balegariw/MainActivity.java
@@ -3,14 +3,23 @@ package com.example.amanu.balegariw;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.design.widget.NavigationView;
+import android.support.transition.Slide;
 import android.support.v4.app.Fragment;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity
         implements BottomNavigationView.OnNavigationItemSelectedListener {
-
+int x = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +27,21 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
 
 
+        ImageButton toggle = findViewById(R.id.menu_button);
+        final DrawerLayout NavDrawer = findViewById(R.id.drawer_layout);
+        toggle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(x == 1){
+                    NavDrawer.openDrawer((int) Gravity.LEFT);
+                    x  = 2;
+                }else{
+                    NavDrawer.closeDrawer((int) Gravity.LEFT);
+                    x  = 1;
+                }
+            }
+
+        });
         BottomNavigationView navigation = findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(this);
 
@@ -40,15 +64,17 @@ public class MainActivity extends AppCompatActivity
         Fragment fragment = null;
         switch (Item.getItemId()){
             case R.id.navigation_home:
-            fragment = new RoutesFragment();
-            break;
+                fragment = new RoutesFragment();
+                break;
             case R.id.navigation_dashboard:
                 fragment = new MapFragment();
                 break;
             case R.id.navigation_notifications:
                 fragment = new MessagesFragment();
-                  break;
+                break;
         }
         return loadFragment(fragment);
     }
+
+
 }
--- app/src/main/java/com/example/amanu/balegariw/MapFragment.java
+++ app/src/main/java/com/example/amanu/balegariw/MapFragment.java
@@ -8,7 +8,11 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+
 public class MapFragment extends Fragment {
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
